05be32e35afa551bef8ebb402a3dd1a2da5bf945
mudur <muhammedemin.dev@gmail.com>
2022-06-08 13:09:45 +0300

    TimeInterval+toString extension added

---
+++ TimeInterval+toString.swift
@@ -0,0 +1,41 @@
+//
+//  TimeInterval+toString.swift
+//  iOSExtensions
+//
+//  Created by Muhammed Emin Aydın on 8.06.2022.
+//
+
+import Foundation
+
+
+extension TimeInterval {
+    private var milliseconds: Int {
+        return Int((truncatingRemainder(dividingBy: 1)) * 1000)
+    }
+
+    private var seconds: Int {
+        return Int(self) % 60
+    }
+
+    private var minutes: Int {
+        return (Int(self) / 60 ) % 60
+    }
+
+    private var hours: Int {
+        return Int(self) / 3600
+    }
+
+    var toString: String {
+        if hours != 0 {
+            return "\(hours)h \(minutes)m \(seconds)s"
+        } else if minutes != 0 {
+            return "\(minutes)m \(seconds)s"
+        } else if milliseconds != 0 {
+            return "\(seconds)s \(milliseconds)ms"
+        } else {
+            return "\(seconds)s"
+        }
+    }
+}
+
+
